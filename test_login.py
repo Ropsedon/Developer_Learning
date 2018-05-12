76b66249e39e8cc3f689b5594abc4f09e564c981
Ropsedon <alexsandr.glukhov@gmail.com>
2018-05-13 02:04:53 +0300

    Add chromiumdriver

--- test_login.py
+++ test_login.py
@@ -14,7 +14,7 @@ class UntitledTestCase(unittest.TestCase):
         self.base_url = "https://www.katalon.com/"
         self.verificationErrors = []
         self.accept_next_alert = True
-    
+
     def test_untitled_test_case(self):
         driver = self.driver
         driver.get("https://www.ukr.net/")
@@ -22,21 +22,21 @@ class UntitledTestCase(unittest.TestCase):
         driver.find_element_by_name("Login").clear()
         driver.find_element_by_name("Login").send_keys("slandoseil@ukr.net")
         driver.find_element_by_name("Password").clear()
-        driver.find_element_by_name("Password").send_keys("1201asdzxc")
+        driver.find_element_by_name("Password").send_keys("1201")
         driver.find_element_by_id("user-login-form").submit()
         driver.find_element_by_link_text(u"Листи").click()
         # ERROR: Caught exception [ERROR: Unsupported command [selectWindow | win_ser_1 | ]]
-    
+
     def is_element_present(self, how, what):
         try: self.driver.find_element(by=how, value=what)
         except NoSuchElementException as e: return False
         return True
-    
+
     def is_alert_present(self):
         try: self.driver.switch_to_alert()
         except NoAlertPresentException as e: return False
         return True
-    
+
     def close_alert_and_get_its_text(self):
         try:
             alert = self.driver.switch_to_alert()
@@ -47,7 +47,7 @@ class UntitledTestCase(unittest.TestCase):
                 alert.dismiss()
             return alert_text
         finally: self.accept_next_alert = True
-    
+
     def tearDown(self):
         self.driver.quit()
         self.assertEqual([], self.verificationErrors)
